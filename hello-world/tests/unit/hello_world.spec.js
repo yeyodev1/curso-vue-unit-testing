ead4cd9b2e314af1f0023483458b94e27717ecb3
Diego Reyes <diegorele13@gmail.com>
2023-04-24 12:56:11 -0500

    pass test

---
+++ hello-world/tests/unit/hello_world.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders hello world component', () => {
+    const wrapper = shallowMount(HelloWorld)
+
+    const component = wrapper.find('.hello')
+
+    expect(component.classes()).toContain('hello')
+  })
+})
